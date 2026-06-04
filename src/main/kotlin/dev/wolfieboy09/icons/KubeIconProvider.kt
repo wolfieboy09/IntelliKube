787e33f99baa12b1ab3986e05cd03faf86664d09
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2026-06-04 19:22:19 -0400

    Add bindings to the list of icons

--- src/main/kotlin/dev/wolfieboy09/icons/KubeIconProvider.kt
+++ src/main/kotlin/dev/wolfieboy09/icons/KubeIconProvider.kt
@@ -6,8 +6,12 @@ import com.intellij.openapi.vfs.VirtualFile
 import javax.swing.Icon
 
 class KubeIconProvider : FileIconProvider {
+
+    // Just to make sure if a custom icon for specific files become a thing, and also to make sure
+    // files that don't start with "kubejs" or contain it get the kube icon
     private val kubeFiles = mapOf(
         "kubejs.plugins.txt" to KubeIcons.DEFAULT,
+        "kubejs.bindings.txt" to KubeIcons.DEFAULT,
     )
 
     override fun getIcon(file: VirtualFile, p1: Int, p2: Project?): Icon? = kubeFiles[file.name]
